8c3d1e7c4feaeba6ed98dca92ad3e1410d1aad6a
Thiago Piassi Della Betta <104688160+ThiagoJoestar@users.noreply.github.com>
2023-05-16 21:37:06 -0300

    Add files via upload
    Atividade Prática 10 - Fundamentos de JavaScript.
    Uso do objeto matemático "Math" com 4 exemplos: 
    Math.sqrt 
    Math.max
    Math.min
    Math.pow

---
+++ AtividadePratica10.js
@@ -0,0 +1,16 @@
+// Math.sqrt
+let raiz = Math.sqrt(4);
+console.log("Raiz quadrade de 4 => " + raiz); 
+
+// Math.max e Math.min
+let num = [2, 4, 6, 8, 10];
+let maior = Math.max(...num);
+let menor = Math.min(...num);
+console.log("O maior número da lista é " + maior);
+console.log("O menor número da lista é " + menor);
+
+// Math.pow
+let base = 3;
+let expoente = 4;
+let potencia = Math.pow(base, expoente);
+console.log(base + " elevado a " + expoente + " ª potência é: " + potencia);
